0ae0e29a0c9f8586aeb3e7988aa0a6244baac573
Rodrigo <49698889+rodrigonepomuceno@users.noreply.github.com>
2022-04-18 14:22:43 -0300

    Example tests

--- test/app/features/news/news_controller_test.dart
+++ test/app/features/news/news_controller_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:news_app/app/core/models/news_model.dart';
 import 'package:news_app/app/core/repositories/news_repository/news_repository_interface.dart';
 import '../../core/repositories/news_repository_mock.dart';
 
@@ -12,7 +13,12 @@ void main() {
   });
   binding.builder();
   final repository = Get.find<NewsRepositoryInterface>();
-  test('Return NewsModel list', () async {
+  test('Should return a NewsModel', () async {
+    final list = await repository.getNews();
+
+    expect(list[0].runtimeType, NewsModel);
+  });
+  test('Should validate the return title of the first element', () async {
     final list = await repository.getNews();
 
     expect(list[0].title, 'Test Mock');
